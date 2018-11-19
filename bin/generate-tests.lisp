ac54a14ab3cfd0ccac0fd017cd7ab0fa06402e7c
Mark Simpson <verdammelt@gmail.com>
2018-11-19 13:07:43 -0500

    WIP 2

--- bin/generate-tests.lisp
+++ bin/generate-tests.lisp
@@ -1,3 +1,11 @@
+;; TODO
+;; - input parameter cleanup
+;; - assert-error
+;; - production code stub
+;; - example code file
+;; - exercise comment in test file
+;; - exercise version number in test file
+
 (ql:quickload "cl-json")
 
 (defpackage :test-generator
@@ -30,45 +38,74 @@
       (with-open-file (stream pathname)
         (cl-json:decode-json-strict stream)))))
 
-(defun write-prologue (stream name)
-  (format stream "~
+(defun write-prologue (stream test-data)
+  (let* ((name (cdr (assoc :exercise test-data)))
+         (test-package (format nil "~a-test" name)))
+    (format stream "~
 (ql:quickload ~w)
 #-xlisp-test (load ~w)
 
 (defpackage #:~a
   (:use #:common-lisp #:lisp-unit))
 
-(in-package #:~:*~a)
-" "lisp-unit" name (format nil "~a-test" name)))
+(in-package #:~a)" "lisp-unit" name test-package test-package))
+  (terpri stream))
 
-(defun write-epilogue (stream)
-  (format stream "~
-#-xlisp-test
+
+(defun write-epilogue (stream test-data)
+  (declare (ignore test-data))
+  (format stream "#-xlisp-test
 (let ((*print-errors* t)
       (*print-failures* t))
-  (run-tests :all)
-"))
+  (run-tests :all))")
+  (terpri stream))
 
-(defun write-tests (stream tests-data)
-  (format stream "~&~&"))
+(defun write-test (stream package case)
+  (let ((description (cdr (assoc :description case)))
+        (property (cdr (assoc :property case)))
+        (input (cdr (assoc :input case)))
+        (expected (cdr (assoc :expected case))))
 
-(defun make-tests (test-data)
+    (format stream "(define-test ~A
+  (assert-equal
+      ~S
+      (~A:~A ~S)))" (substitute #\- #\space description)
+      expected package property input))
+
+  (terpri stream))
+
+
+(defun write-tests (stream test-data)
+  (let ((cases (cdr (assoc :cases test-data)))
+        (package (cdr (assoc :exercise test-data))))
+    (dolist (case cases) (write-test stream package case))))
+
+(defun make-exercise-directory (test-data)
+  (ensure-directories-exist
+   (exercise-directory-pathname (cdr (assoc :exercise test-data)))))
+
+(defun make-test-code (test-data)
   (let* ((exercise (cdr (assoc :exercise test-data)))
          (exercise-directory (exercise-directory-pathname exercise))
          (test-file (make-pathname :name (format nil "~A-test" exercise)
                                    :type "lisp"
                                    :defaults exercise-directory)))
-    (ensure-directories-exist exercise-directory :verbose t)
     (with-open-file (stream test-file
                             :direction :output
                             :if-does-not-exist :create
                             :if-exists :supersede)
-      (write-prologue stream exercise)
+      (write-prologue stream test-data)
       (write-tests stream test-data)
-      (write-epilogue stream))))
+      (write-epilogue stream test-data))))
+
+(defun make-production-code (test-data)
+  (declare (ignore test-data)))
 
 (defun generate (exercise)
   (let ((test-data (canonical-data exercise)))
     (if test-data
-        (make-tests test-data)
-        (format t "No data foun for exercise: ~A" exercise))))
+        (progn
+          (make-exercise-directory test-data)
+          (make-test-code test-data)
+          (make-production-code test-data))
+        (format t "No data found for exercise: ~A" exercise))))
