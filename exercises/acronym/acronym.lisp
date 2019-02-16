979939f38006b193e969aef4c88524f367ec47da
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-02-16 01:59:56 -0500

    Working on acronym function, currently getting nil for value that should be non-nil.

--- exercises/acronym/acronym.lisp
+++ exercises/acronym/acronym.lisp
@@ -1,6 +1,15 @@
-(in-package #:cl-user)
-(defpackage #:acronym
-  (:use #:common-lisp)
-  (:export #:acronym))
+; (in-package #:cl-user)
+; (defpackage #:acronym
+;   (:use #:common-lisp)
+;   (:export #:acronym))
+;
+; (in-package #:acronym)
 
-(in-package #:acronym)
+(defun acronym (lst)
+ (cond ((eql (length '(lst)) nil) (nil))
+       ((eql (length '(lst)) 1)
+        (subseq (car lst) 0 1))
+       ((> (length '(lst) 1)
+           (dolist (x (length lst))
+                  (concatenate 'string
+                               (subseq (car lst) 0 1)))))))
